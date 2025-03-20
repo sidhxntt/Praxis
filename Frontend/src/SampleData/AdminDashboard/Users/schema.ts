621c3714127c67cfd4906d92cc43d8ac482c0306
Sid <siddhantg2002@gmail.com>
2025-03-20 23:21:57 +0530

    fullstack -2

--- Frontend/src/SampleData/AdminDashboard/Users/schema.ts
+++ Frontend/src/SampleData/AdminDashboard/Users/schema.ts
@@ -17,16 +17,16 @@ const userRoleSchema = z.union([
 
 const userSchema = z.object({
   id: z.string(),
-  firstName: z.string(),
-  lastName: z.string(),
+  firstName: z.string().nullable(),
+  lastName: z.string().nullable(),
   username: z.string(),
-  email: z.string(),
-  phoneNumber: z.string(),
+  email: z.string().nullable(),
+  phoneNumber: z.string().nullable(),
   status: userStatusSchema,
   role: userRoleSchema,
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
-})
+});
 export type User = z.infer<typeof userSchema>
 
 export const userListSchema = z.array(userSchema)
