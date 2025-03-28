2f8792dba2775bde5abb19b12024d41b69e376df
Sid <siddhantg2002@gmail.com>
2025-03-28 11:22:31 +0530

    some changes

--- js-next-template/src/SampleData/AdminDashboard/Users/schema.js
+++ js-next-template/src/SampleData/AdminDashboard/Users/schema.js
@@ -27,4 +27,64 @@ const userSchema = z.object({
   updatedAt: z.coerce.date(),
 })
 
+//form
+export const formSchema = z
+  .object({
+    firstName: z.string().min(1, { message: "First Name is required." }),
+    lastName: z.string().min(1, { message: "Last Name is required." }),
+    username: z.string().min(1, { message: "Username is required." }),
+    phoneNumber: z.string().min(1, { message: "Phone number is required." }),
+    email: z
+      .string()
+      .min(1, { message: "Email is required." })
+      .email({ message: "Email is invalid." }),
+    password: z.string().transform((pwd) => pwd.trim()),
+    role: z.string().min(1, { message: "Role is required." }),
+    confirmPassword: z.string().transform((pwd) => pwd.trim()),
+    isEdit: z.boolean(),
+  })
+  .superRefine(({ isEdit, password, confirmPassword }, ctx) => {
+    if (!isEdit || (isEdit && password !== "")) {
+      if (password === "") {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Password is required.",
+          path: ["password"],
+        });
+      }
+
+      if (password.length < 8) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Password must be at least 8 characters long.",
+          path: ["password"],
+        });
+      }
+
+      if (!password.match(/[a-z]/)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Password must contain at least one lowercase letter.",
+          path: ["password"],
+        });
+      }
+
+      if (!password.match(/\d/)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Password must contain at least one number.",
+          path: ["password"],
+        });
+      }
+
+      if (password !== confirmPassword) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: "Passwords don't match.",
+          path: ["confirmPassword"],
+        });
+      }
+    }
+  });
+
 export const userListSchema = z.array(userSchema)
