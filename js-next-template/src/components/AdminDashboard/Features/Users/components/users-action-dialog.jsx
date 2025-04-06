b3682d69706dce42abd09053fa0c0af732e1bcfa
Sid <siddhantg2002@gmail.com>
2025-04-06 13:37:31 +0530

    changes

--- js-next-template/src/components/AdminDashboard/Features/Users/components/users-action-dialog.jsx
+++ js-next-template/src/components/AdminDashboard/Features/Users/components/users-action-dialog.jsx
@@ -3,7 +3,7 @@
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { toast } from "@/temp/use-toast";
+import { toast } from "@/Hooks/use-toast";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
